376221697c32469cc5c6e9ae6e335d5afc5960c1
pmun966 <guseiei65@gmail.com>
2026-07-28 03:28:15 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,13 @@ import aiosqlite
 
 DATABASE = "data/auction.db"
 
+
 async def setup_database():
     async with aiosqlite.connect(DATABASE) as db:
 
         await db.execute("""
         CREATE TABLE IF NOT EXISTS auctions(
+
             id INTEGER PRIMARY KEY AUTOINCREMENT,
 
             guild_id INTEGER,
@@ -21,6 +23,7 @@ async def setup_database():
 
             start_price INTEGER,
             current_price INTEGER,
+
             minimum_bid INTEGER,
 
             highest_bidder INTEGER,
@@ -30,5 +33,90 @@ async def setup_database():
             status TEXT
         )
         """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS bids(
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            auction_id INTEGER,
+
+            user_id INTEGER,
+
+            amount INTEGER,
+
+            bid_time INTEGER
+        )
+        """)
+
+        await db.commit()
+
+
+async def get_active_auction(channel_id):
+
+    async with aiosqlite.connect(DATABASE) as db:
+
+        db.row_factory = aiosqlite.Row
+
+        cursor = await db.execute(
+            "SELECT * FROM auctions WHERE channel_id=? AND status='OPEN'",
+            (channel_id,)
+        )
+
+        return await cursor.fetchone()
+
+
+async def add_bid(
+    auction_id,
+    user_id,
+    amount,
+    bid_time
+):
+
+    async with aiosqlite.connect(DATABASE) as db:
+
+        await db.execute(
+            """
+            INSERT INTO bids(
+                auction_id,
+                user_id,
+                amount,
+                bid_time
+            )
+            VALUES(?,?,?,?)
+            """,
+            (
+                auction_id,
+                user_id,
+                amount,
+                bid_time
+            )
+        )
+
+        await db.commit()
+
+
+async def update_price(
+    auction_id,
+    price,
+    bidder
+):
+
+    async with aiosqlite.connect(DATABASE) as db:
+
+        await db.execute(
+            """
+            UPDATE auctions
+            SET
+                current_price=?,
+                highest_bidder=?
+            WHERE id=?
+            """,
+            (
+                price,
+                bidder,
+                auction_id
+            )
+        )
 
         await db.commit()
